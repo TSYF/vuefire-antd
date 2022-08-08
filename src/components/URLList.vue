117fec2ec53a0694f895741333e4f56c52b4cf08
TSYF <tsyf1999@gmail.com>
2022-08-07 22:53:06 -0400

    added URLList component

---
+++ src/components/URLList.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<p v-if="URLStore.loadingDocs">Loading...</p>
+		<ul v-else>
+			<li v-for="document of documents" :key="document.id">
+				<pre>
+ID: {{ document.id }}
+NAME: {{ document.name }}
+SHORT:{{ document.short }}
+USER: {{ document.user }}
+				</pre
+				>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+import { useURLStore } from "@/stores/urls.js";
+import { computed } from "vue";
+
+const URLStore = useURLStore();
+
+const documents = computed(() => URLStore.documents);
+
+URLStore.getURLS();
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,13 @@
 		<h3>{{ userStore.user.email }}</h3>
 		<h3>{{ userStore.user.uid }}</h3>
 		<br />
+		<URLList />
 	</div>
 </template>
 
 <script setup>
 import { useUserStore } from "@/stores/user.js";
+import URLList from "../components/URLList.vue";
 
 const userStore = useUserStore();
 </script>
