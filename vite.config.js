93221ec453cff57327b19fdb5d847b8c3ce9299a
TSYF <tsyf1999@gmail.com>
2022-08-21 23:15:27 -0400

    modified vite-config to fix an ant design test problem

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
+/// <reference types="vitest"/>
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
+import { defineConfig } from 'vite'
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 	plugins: [
 		vue(),
 		Components({
 			resolvers: [AntDesignVueResolver()],
 		}),
 	],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+	test: {
+		deps: {
+			inline: [
+				"ant-design-vue"
+			]
 		},
-	},
+	}
 });
