03bab3873ac031d29b53d66f01a9eb057c960d59
TSYF <tsyf1999@gmail.com>
2022-08-23 14:46:21 -0400

    Beautified EditURL component the ANTD way, added Firestore error handling

--- src/components/EditURL.vue
+++ src/components/EditURL.vue
@@ -1,18 +1,44 @@
 <template>
-	<form @submit.prevent="handleSubmit()">
-		<h3>Editar URL</h3>
-		<input
-			v-model="url"
-			type="text"
-			placeholder="Insert URL"
-			name="url_edit"
-		/>
-		<button type="submit">Edit</button>
-	</form>
+	<a-form
+		name="editForm"
+		:model="formState"
+		layout="vertical"
+		@finish="handleSubmit()"
+	>
+		<a-form-item
+			name="editURL"
+			:label="'Edit URL ' + doc.name + ':'"
+			style="max-width: 40%"
+			:rules="{
+				required: true,
+				whitespace: true,
+				message: 'Enter a valid URL',
+				pattern: URLStore.regex
+			}"
+		>
+			<a-input
+				v-model:value="formState.url"
+				type="text"
+				placeholder="URL"
+				name="url_edit"
+			/>
+		</a-form-item>
+		<a-row>
+			<a-form-item style="margin-right: 1rem;">
+				<a-button
+					html-type="submit"
+					type="primary"
+				>Edit</a-button>
+			</a-form-item>
+			<a-form-item>
+				<a-button @click="doc.editing = false">Cancel</a-button>
+			</a-form-item>
+		</a-row>
+	</a-form>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from 'vue';
 import { useURLStore } from "@/stores/urls.js";
 
 const URLStore = useURLStore();
@@ -21,10 +47,14 @@ const { doc } = defineProps({
 	doc: Object,
 });
 
-const url = ref("");
-
 const handleSubmit = () => {
-	URLStore.editURL(doc.id, url.value);
-	url.value = "";
+	URLStore.editURL(doc.id, formState.url)
+		.then((res) => formState.url = "" || res)
+		.then(() => message.success("URL Edited!"))
+		.catch(() => message.error("Something went wrong... :c"));
 };
+
+const formState = reactive({
+	url: ""
+})
 </script>
