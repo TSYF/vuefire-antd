e535ff8193ab3b8d1deba92e83ac1e8065c91f62
TSYF <tsyf1999@gmail.com>
2022-08-23 15:54:40 -0400

    ANTD URLList with cards and confirmation before deletion

--- src/components/URLList.vue
+++ src/components/URLList.vue
@@ -1,18 +1,53 @@
 <template>
-	<div>
+	<div style="margin-top: 1rem">
 		<p v-if="URLStore.loadingDocs">Loading...</p>
-		<ul v-else>
-			<li v-for="document of documents" :key="document.id">
-				<pre v-if="!document.editing">
-ID: {{ document.id }}
-NAME: {{ document.name }}
-SHORT:{{ document.short }}
-USER: {{ document.user }}
-<button @click="URLStore.deleteURL(document.id)">Delete</button>
-<button @click="document.editing = true">Edit</button>
-				</pre>
-				<EditURL v-else :doc="document" />
-			</li>
+		<ul
+			v-else
+			style="list-style-type: none; padding: 0;"
+		>
+			<a-space direction="vertical">
+				<li
+					v-for="document of documents"
+					:key="document.id"
+				>
+					<a-card :title="'URL: ' + document.name">
+						<template #extra>
+							<a-space>
+								<a-popconfirm
+									danger
+									@confirm="deleteConfirm(document.id)"
+									@cancel="deleteCancel()"
+									title="Are you sure about that?"
+								>
+									<template #icon>
+										<question-circle-outlined style="color: red" />
+									</template>
+									<a-button
+										danger
+										:disabled="URLStore.loadingCDDocs"
+										:loading="URLStore.loadingCDDocs"
+									>
+										Delete
+									</a-button>
+								</a-popconfirm>
+								<a-button
+									type="primary"
+									@click="document.editing = true"
+								>
+									Edit
+								</a-button>
+							</a-space>
+						</template>
+						<p v-if="!document.editing">ID: {{ document.id }}</p>
+						<p v-if="!document.editing">SHORT:{{ document.short }}</p>
+						<p v-if="!document.editing">USER: {{ document.user }}</p>
+						<EditURL
+							v-else
+							:doc="document"
+						/>
+					</a-card>
+				</li>
+			</a-space>
 		</ul>
 	</div>
 </template>
@@ -21,10 +56,23 @@ USER: {{ document.user }}
 import { useURLStore } from "@/stores/urls.js";
 import { computed } from "vue";
 import EditURL from "./EditURL.vue";
+import { QuestionCircleOutlined } from "@ant-design/icons-vue";
+import { message } from "ant-design-vue";
 
 const URLStore = useURLStore();
 
 const documents = computed(() => URLStore.documents);
 
 URLStore.getURLS();
+
+const deleteConfirm = async id => {
+	URLStore.deleteURL(id)
+		.then(_ => message.success("URL Successfully Deleted"))
+		.catch(_ => message.error("Something went wrong... :c"))
+}
+
+const deleteCancel = _ => message.info("Deletion Cancelled")
 </script>
+
+<style scoped>
+</style>
