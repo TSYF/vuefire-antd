0e7f32f1a89fc0ae62e929692dcdf40744577b27
TSYF <tsyf1999@gmail.com>
2023-03-22 21:16:04 -0300

    Changed HomeView title to "Content List" and eliminated user info that was being displayed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <template>
 	<div>
-		<h1>Hellow</h1>
-		<h3>{{ userStore.user.email }}</h3>
-		<h3>{{ userStore.user.uid }}</h3>
+		<h1>Content List</h1>
 		<br />
 		<URLForm />
 		<URLList />
@@ -10,11 +8,9 @@
 </template>
 
 <script setup>
-import { useUserStore } from "@/stores/user.js";
 import URLList from "../components/URLList.vue";
 import URLForm from "../components/URLForm.vue";
-
-const userStore = useUserStore();
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
