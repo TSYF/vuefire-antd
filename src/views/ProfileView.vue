7c13e9923fbed2c8e8c0dea74b3b4258c9650801
TSYF <tsyf1999@gmail.com>
2023-03-22 21:13:10 -0300

    Created ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <h1>Perfil de Usuario</h1>
+        <h1>Profile</h1>
+        <h2>{{ user.displayName }}</h2>
+        <img :src="user.photoURL" alt="Profile Photo">
+        <br>
+        <em>{{ user.email }}</em>
     </div>
 </template>
 
 <script setup>
+import { useUserStore } from "@/stores/user.js";
+
+const { user } = useUserStore();
+
 </script>
 
 <style lang='scss' scoped>
